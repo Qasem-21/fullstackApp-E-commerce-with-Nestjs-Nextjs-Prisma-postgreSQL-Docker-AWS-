742bd5303c3fba5411a5eb1a988709611689787b
Qasem Mohammadi <qasimmohammadi520@gmail.com>
2026-02-20 22:34:57 +0430

    using global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 // import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // project description
   app.setGlobalPrefix('api/v1');
+
+  // set Global validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3010);
 }
 bootstrap().catch((error) => {
