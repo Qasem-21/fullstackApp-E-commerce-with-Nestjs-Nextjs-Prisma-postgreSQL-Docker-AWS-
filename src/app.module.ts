6f0d63f29ce47c4f892d2aba18efe8fbe1cef170
Qasem Mohammadi <qasimmohammadi520@gmail.com>
2026-04-17 10:38:21 +0430

    basic setup(module, controller and service) for order apis

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module.js';
 import { CategoryModule } from './modules/category/category.module';
 import { ProductsModule } from './modules/products/products.module';
+import { OrdersModule } from './modules/orders/orders.module';
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { ProductsModule } from './modules/products/products.module';
     UsersModule,
     CategoryModule,
     ProductsModule,
+    OrdersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
